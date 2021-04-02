60acb43643db80836a8d65c9fda23d007593d14f
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-04-02 21:06:37 +0500

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,8 +1,8 @@
 """
-
-The main module calls these functions with the arguments provided at the command
-line, and uses the resulting collections to build an `NEODatabase`.
-
+The main module calls these functions with the
+arguments provided at the command
+line, and uses the resulting collections
+to build an `NEODatabase`.
 """
 import csv
 import json
@@ -19,22 +19,22 @@ def load_neos(neo_csv_path):
     with open(neo_csv_path, 'r') as file:
         reader = DictReader(file)
         for x in reader:
-            result.append(NearEarthObject(designation = x['pdes'], name = x['name'], 
-                            hazardous = x['pha'], diameter = x['diameter']))
+            result.append(NearEarthObject(designation=x['pdes'],
+                                          name=x['name'],
+                                          hazardous=x['pha'],
+                                          diameter=x['diameter']))
 
     return result
-
 
 
 def load_approaches(cad_json_path):
     """Read close approach data from a JSON file.
     """
-
-
     bucket = []
     with open(cad_json_path, 'r') as file2:
         data = json.load(file2)
         for x in data['data']:
-            bucket.append(CloseApproach(x[0], time = x[3], 
-                                        distance = float(x[4]), velocity = float(x[7])))
+            bucket.append(CloseApproach(x[0], time=x[3],
+                                        distance=float(x[4]),
+                                        velocity=float(x[7])))
     return bucket

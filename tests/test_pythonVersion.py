49cc5deccdc78ddcd1c59228900a9a5f25e17a15
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-04-14 14:58:08 +0500

    Update test_pythonVersion.py

--- tests/test_pythonVersion.py
+++ tests/test_pythonVersion.py
@@ -6,9 +6,8 @@ class VersionTest(unittest.TestCase):
 	'''Python version check-up'''
 	def test_version(self):
 		self.assertTrue(sys.version_info > (3, 5), f"major: {sys.version_info.major}, "
-												   f"minor: {sys.version_info.minor}, "
-												   f"micro: {sys.version_info.micro}"
-													)
+							   f"minor: {sys.version_info.minor}, "
+							   f"micro: {sys.version_info.micro}")
 
 # sys.version_info(major=3, minor=7, micro=9, releaselevel='final', serial=0)
 
